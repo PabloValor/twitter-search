da5d62b2cbd8f70d83843b37bd4e369c6ed95f17
Pablo Valor <pablovalor89@gmail.com>
2015-07-18 20:06:24 -0300

    Added server file. Added Twitter and Express

--- src/js/twitter-search.js
+++ src/js/twitter-search.js
@@ -1,1 +1,8 @@
-console.log("working");
+$(document).on('ready', function(){
+    "use strict";
+    var $searchInput = $('#search');
+
+    $searchInput.focus();
+
+    console.log("Working...");
+});
