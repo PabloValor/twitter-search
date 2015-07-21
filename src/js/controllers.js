eeddd756356e9f2f20398d58754ee2791f334a53
Pablo Valor <pablovalor89@gmail.com>
2015-07-21 00:26:27 -0300

    changed colors. Added momentjs

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -4,12 +4,14 @@
 
     .controller('twitterSearchController', ['$scope','twitterSearchService', function($scope, twitterSearchService) {
         $scope.submit = function(search){
+            $scope.tweets = [];
             $scope.progressBarFlag = true; //show the progress bar while request is sending
 
             twitterSearchService.getTweets(search)
                 .then(function(tweets){
                     $scope.tweets = tweets.statuses;
                     $scope.progressBarFlag = false;
+                    console.log($scope.tweets);
                 })
         }
     }]);
--- src/js/twitter-search.js
+++ src/js/twitter-search.js
@@ -2,6 +2,23 @@ $(document).on('ready', function(){
     "use strict";
     var $searchInput = $('#search');
 
-    $searchInput.focus();
-    console.log("Works...");
+    // setup and configure scrollFire Materialize feature
+    /*var options = [{
+        selector: '#tweetsList',
+        offset: 200,
+        callback: 'Materialize.showStaggeredList("#tweetsList")'
+    }];
+    // scrollFire starts
+    Materialize.scrollFire(options);*/
+
+    // initialize Materialize tooltip
+    $('#btnSearch').on('click', function(){
+        // dirty hack to apply tooltip functionallity in dom elements
+        setTimeout(function(){
+            $('.tooltipped').tooltip({delay: 50});
+
+        },2500);
+    });
+
+    console.log("dom ready...");
 });
